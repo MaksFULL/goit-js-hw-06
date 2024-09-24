637e3c731938cdd876ba090ae04d034dc42806dc
MaksFULL <169079873+MaksFULL@users.noreply.github.com>
2024-09-24 21:01:10 +0300

    homework6

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,33 @@
-// Стрілочна функція для сортування користувачів за спаданням кількості друзів
-const sortByDescendingFriendCount = (users) => 
-  users.toSorted((a, b) => b.friends.length - a.friends.length);
+class StringBuilder {
+  #value; 
 
-// Перевірка коректності роботи функції
-console.log(
-  sortByDescendingFriendCount([
-    { name: "Moore Hensley", friends: ["Sharron Pace"], gender: "male" },
-    { name: "Sharlene Bush", friends: ["Briana Decker", "Sharron Pace"], gender: "female" },
-    { name: "Ross Vazquez", friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"], gender: "male" },
-    { name: "Elma Head", friends: ["Goldie Gentry", "Aisha Tran"], gender: "female" },
-    { name: "Carey Barr", friends: ["Jordan Sampson", "Eddie Strong"], gender: "male" },
-    { name: "Blackburn Dotson", friends: ["Jacklyn Lucas", "Linda Chapman"], gender: "male" },
-    { name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"], gender: "female" }
-  ])
-);
-// Сортування поверне масив користувачів, відсортованих за кількістю друзів за спаданням
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); 
+builder.padStart("^");
+console.log(builder.getValue()); 
+builder.padEnd("^");
+console.log(builder.getValue()); 
+builder.padBoth("=");
+console.log(builder.getValue()); 
